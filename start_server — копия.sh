28cb797ddf17ddf61b31450add391500644f3020
Валерия Низоля <leranizolya@yandex.ru>
2021-03-28 18:40:39 +0300

    Final2

--- start_server — копия.sh
+++
@@ -1,5 +0,0 @@
-## При установке на стороннюю ВМ нужно прописать путь к flask окружению для работы
-source /home/entrant/venv/myproject/bin/activate;
-cd /home/entrant/rest-api-yandex;
-gunicorn --bind 0.0.0.0:8080 main:app
-
